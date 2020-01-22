fdd8c35568e2fffa8df509e25e88a95e63f3e4f7
Mohit Chhabra <chhabra@medialesson.de>
2020-01-22 17:07:40 +0100

    Add project files.

---
+++ FunctionApp4/Function1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+
+namespace FunctionApp4
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            var secret = await keyVaultClient.GetSecretAsync("https://mlkvdemo1.vault.azure.net/secrets/Kvsecret").ConfigureAwait(false);
+            return secret.Value != null
+                ? (ActionResult)new OkObjectResult($"{secret.Value}")
+                : new BadRequestObjectResult("Could not find a secret");
+        }
+    }
+}
